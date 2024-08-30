c066f190bfb6277256adc5d8846c3b5fb17051de
aditya <112795067+AdityaBagul12345@users.noreply.github.com>
2024-08-30 23:29:53 +0530

    Delete bookStore directory

--- bookStore/backend/route/bookRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const getBook = require('../controllers/book.controller.js')
-
-const router = express.Router();
-router.get('/',getBook);
-
-module.exports = router;
-
